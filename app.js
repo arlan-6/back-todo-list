83e82362af310eff15a26198659f11e9ee853dd5
arlan-6 <119741699+arlan-6@users.noreply.github.com>
2023-10-26 12:56:53 +0600

    fix cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 
 import noteModel from "./models/note.js";
+import cors from "cors";
 
 mongoose
   .connect(
@@ -16,7 +17,18 @@ mongoose
   });
 
 const app = express();
-
+const allowedOrigins = ["http://localhost:5173"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use(express.json());
 
 app.post("/note", async (req, res) => {
